62f6fe84a2539e58ba313aaf4c3c4b6b310c2f9e
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-11 12:22:51 -0300

    Products.json

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const getProducts = async () => {
+
+    try {
+        const res = await fetch('./data/products.json')
+        const data = await res.json()
+        const products = data.products
+        console.log(products)
+
+        return products
+    } catch(error) {
+        console.error('Hove um erro ao carregar os produtos...')
+        console.error(error)
+    }
+}
+
+const ProductWrapper = document.getElementById('products')
+
+window.addEventListener('DOMContentLoaded', async function() {
+    let products = await getProducts()
+
+    products = products.filter(product => product.category === 'filter-sneaker')
+    displayProductItems(products)
+})
+
+const displayProductItems = items => {
+    let displayProduct = items.map(product => 
+
+        ` <div class="product-top flex1">
+            <div class="product-image">
+                <img src=${product.url} alt=${product.title}>
+            </div>
+            <div class="product-icon">
+                <i class="uil uil-heart"></i>
+            </div>
+         </div>
+         <div class="product-bottom">
+            <h1>${product.title}</h1>
+            <div class="product-tating">
+                <i class="uil uil-star"></i>
+                <i class="uil uil-star"></i>
+                <i class="uil uil-star"></i>
+                <i class="uil uil-star"></i>
+                <i class="uil uil-star"></i>
+            </div>
+            <div class="product-price">
+                <div class="price">R$ ${product.price}</div>
+            </div>
+         </div> `
+    );
+
+    displayProduct = displayProduct.join("")
+    ProductWrapper.innerHTML = displayProduct;
+}
